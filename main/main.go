1f3dfc9ba90747876cfe27ac44fb22b36ed2a879
qq4417qq <1206153879@qq.com>
2024-05-13 19:44:48 +0800

    增加了服务注册的代码

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"geerpc"
 	"log"
 	"net"
@@ -9,7 +8,20 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := geerpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -21,25 +33,25 @@ func startServer(addr chan string) {
 }
 
 func main() {
-	log.SetFlags(0)                         //清除每个日志条目前加上日期、时间和文件的名称及行号
-	addr := make(chan string)               //addr是一个string类型的通道
-	go startServer(addr)                    //创建服务器
-	client, _ := geerpc.Dial("tcp", <-addr) //直到接受到addr再产生conn
-	defer func() { _ = client.Close() }()   //关闭client
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
 	// send request & receive response
-	var wg sync.WaitGroup //加入wg用来go计数
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
-			if err := client.Call("Foo.Sum", args, &reply); err != nil { //call，先send，再Write，再
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
